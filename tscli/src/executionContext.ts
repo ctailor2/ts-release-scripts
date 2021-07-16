73a4aa36c402e1821e99e7bae54a456b5272c65d
Chirag Tailor <ctailor2@gmail.com>
2021-07-16 08:55:45 -0500

    log stderr as well

--- tscli/src/executionContext.ts
+++ tscli/src/executionContext.ts
@@ -1,11 +1,18 @@
 import { exec } from "child_process";
+import { stderr } from "process";
 
 export interface ExecutionContext {
     sh: (command: string) => void;
     git: (command: string) => void;
 }
 
-const sh = (command: string) => exec(command, (_error, stdout, _stderr) => console.log(stdout));
+const sh = (command: string) => exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.error(error);
+    }
+    console.log('error: ', stderr);
+    console.log(stdout);
+});
 
 const executionContext = {
     sh,
