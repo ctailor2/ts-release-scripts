9276d4c7dc37817e979c602b99052dd92772a53c
Chirag Tailor <ctailor2@gmail.com>
2021-07-16 09:01:42 -0500

    exit with nonzero status upon error

--- tscli/src/executionContext.ts
+++ tscli/src/executionContext.ts
@@ -8,9 +8,9 @@ export interface ExecutionContext {
 
 const sh = (command: string) => exec(command, (error, stdout, stderr) => {
     if (error) {
-        console.error(error);
+        console.error(stderr);
+        process.exit(1);
     }
-    console.log('error: ', stderr);
     console.log(stdout);
 });
 
